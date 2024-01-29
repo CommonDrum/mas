36237f0e94b4fc009bb4e1773473425464bbc890
commondrum <karoljhetman@gmail.com>
2024-01-29 01:27:35 +0000

    Up to date with lecture

--- src/agt/alice.asl
+++ src/agt/alice.asl
@@ -1,11 +1,11 @@
-// Agent alice in project mas
 
-/* Initial beliefs and rules */
+distance(X,Y,Z) :- .substring(NS1,X,4) & .term2string(N1,NS1) & .substring(NS2,Y,4) & .term2string(N2,NS2) & Z=N2-N1.
 
-/* Initial goals */
++location(waste,X) : location(robot,X) & location(bin,Y) & distance(X,Y,Z) & .print("Distance from ",X," to ",Y," is ",Z)
+<-
+    
+    pick(waste);
+    move(robot,Y);
+    drop(waste).
 
-!start.
 
-/* Plans */
-
-+!start : true <- .print("hello world.").
--- src/env/example/Env.java
+++ src/env/example/Env.java
@@ -1,50 +1,49 @@
 package example;
 
-// Environment code for project traffic
-
-import jason.asSyntax.*;
-import jason.environment.*;
-import jason.asSyntax.parser.*;
-
-import java.util.logging.*;
+import jason.asSyntax.Literal;
+import jason.asSyntax.Structure;
+import jason.environment.Environment;
 
 public class Env extends Environment {
+    String robotLocation = "lane1";
+    String binLocation = "lane5";
+    String wasteLocation = "lane1";
 
-    private Logger logger = Logger.getLogger("traffic."+Env.class.getName());
-
-    /** Called before the MAS execution with the args informed in .mas2j */
     @Override
     public void init(String[] args) {
-        super.init(args);
-        try {
-            addPercept(ASSyntax.parseLiteral("location(robot,lane1)"));
-            addPercept(ASSyntax.parseLiteral("location(waste,lane1)"));
-            addPercept(ASSyntax.parseLiteral("location(bin,lane5)"));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        // initial percepts
+        addPercept(Literal.parseLiteral("location(robot,lane1)"));
+        addPercept(Literal.parseLiteral("location(bin,lane5)"));
+        addPercept(Literal.parseLiteral("location(waste,lane1)"));
     }
 
+    /**
+     * Implementation of the agent's basic actions
+     */
     @Override
-    public boolean executeAction(String agName, Structure action) {
-        logger.info("executing: "+action+", but not implemented!");
-        
-        try {
-            if(action.getFunctor().equals("pick")){
-                removePercept(ASSyntax.parseLiteral("location(waste,lane1)"));
-                addPercept(ASSyntax.parseLiteral("location(waste,robot)"));
-            }
-        } catch (ParseException e) {
-            e.printStackTrace();
+    public boolean executeAction(String ag, Structure act) {
+        System.out.println("Agent "+ag+" is doing "+act);
+
+        if (act.getFunctor().equals("pick")) {
+            wasteLocation = "robot";
+            removePercept(Literal.parseLiteral("location(waste,lane1)"));
+            addPercept(Literal.parseLiteral("location(waste,robot)"));
         }
 
+        else if (act.getFunctor().equals("move")) {
+            removePercept(Literal.parseLiteral("location(robot,"+robotLocation+")"));
+            robotLocation = act.getTerm(1).toString();
+            addPercept(Literal.parseLiteral("location(robot,"+act.getTerm(1).toString()+")"));
+        }
+
+        else if (act.getFunctor().equals("drop")) {
+            removePercept(Literal.parseLiteral("location(waste,"+wasteLocation+")"));
+            wasteLocation = null;
+            System.out.println("Waste dropped in the bin");
+        }
+            
         return true;
     }
 
-
-    /** Called before the end of MAS execution */
-    @Override
-    public void stop() {
-        super.stop();
-    }
 }
+
